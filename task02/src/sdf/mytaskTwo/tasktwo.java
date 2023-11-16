9ee2b46e2d5e3d0571f2937f8bfcf17a22041374
pinkskyxx <apryllimfyp@gmail.com>
2023-11-16 16:50:34 +0800

    16Nov2023VTTP

--- task02/src/sdf/mytaskTwo/tasktwo.java
+++ task02/src/sdf/mytaskTwo/tasktwo.java
@@ -1,5 +1,5 @@
 package sdf.mytaskTwo;
- 
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -17,33 +17,30 @@ import java.util.Locale.Category;
 import java.util.Map;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-  
+
 public class tasktwo {
     static Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?"); // check if pattern numeric for delete section
- 
+
     static boolean isNumeric(String strNum) {
         if (strNum == null) {
             return false;
         }
         return pattern.matcher(strNum).matches();
     }
- 
+
     static Map<Integer, List<Items>> Items_receive;
- 
+
     public static void main(String[] args) throws Exception {
         System.out.println("tasktwo");
-        // request_id: 01HFBANPZE69HBM5J09WESZPEY
-        // item_count: 14
-        // budget: 1902.00
- 
+
         String request_id = "";
         int item_count = 0;
         float budget = 0;
- 
+
         Integer port = 3000;
         String Server_Name = "localhost";
         List<String> Items_receive_seq = new ArrayList<>();
- 
+
         switch (args.length) {
             case 0:
                 port = 3000;
@@ -58,7 +55,7 @@ public class tasktwo {
                 }
                 break;
             case 2:
-                // cookieFile = args[0];
+
                 Server_Name = args[0];
                 boolean chk1 = isNumeric(args[1]);
                 if (chk1 == true) {
@@ -71,46 +68,35 @@ public class tasktwo {
             default:
                 System.err.println("Argument error");
                 System.exit(1);
-        } // end switch
- 
+        }
+
         Socket socket = new Socket(Server_Name, port);
- 
+
         InputStream is = socket.getInputStream();
         InputStreamReader isr = new InputStreamReader(is);
         BufferedReader br = new BufferedReader(isr);
- 
+
         OutputStream os = socket.getOutputStream();
         OutputStreamWriter osw = new OutputStreamWriter(os);
         BufferedWriter bw = new BufferedWriter(osw);
         System.out.println("Client Receive");
- 
-        // request_id: 01HFBANPZE69HBM5J09WESZPEY
-        // item_count: 14
-        // budget: 1902.00
-        // prod_list
-        // prod_start
-        // prod_id: 17
-        // title: Rain Jacket Women Windbreaker Striped Climbing Raincoats
-        // price: 39.99
-        // rating: 3.80
-        // prod_end
- 
+
         boolean receiveing = true;
         while (receiveing) {
             String line = br.readLine();
             System.out.println(line);
- 
+
             String userCommand = line.trim().replaceAll(":", " ");// .replaceAll("\\s+", " ");
             String[] terms = userCommand.split("\\s+");
             switch (terms[0]) {
                 case "request_id":
                     request_id = terms[1];
                     break;
- 
+
                 case "item_count":
                     item_count = Integer.parseInt(terms[1]);
                     break;
- 
+
                 case "budget":
                     budget = Float.parseFloat(terms[1]);
                     break;
@@ -121,7 +107,7 @@ public class tasktwo {
                     break;
             }
         }
- 
+
         receiveing = true;
         boolean started = false;
         String prod_id = "";
@@ -132,18 +118,13 @@ public class tasktwo {
         try (Writer fileWriter = new FileWriter(fname, false)) {
             fileWriter.append("");
         }
-        for(int i=0; i<item_count; i++) {
+        for (int i = 0; i < item_count; i++) {
             String line = br.readLine();
             System.out.println(line);
- 
+
             String userCommand = line.trim().replaceAll(":", " ");// .replaceAll("\\s+", " ");
             String[] terms = userCommand.split("\\s+");
-            // prod_start
-            // prod_id: 17
-            // title: Rain Jacket Women Windbreaker Striped Climbing Raincoats
-            // price: 39.99
-            // rating: 3.80
-            // prod_end
+
             switch (terms[0]) {
                 case "prod_start":
                     started = true;
@@ -151,7 +132,7 @@ public class tasktwo {
                 case "prod_id":
                     prod_id = terms[1];
                     break;
- 
+
                 case "title":
                     title = terms[1];
                     break;
@@ -161,11 +142,11 @@ public class tasktwo {
                 case "rating":
                     rating = terms[1];
                     break;
- 
+
                 case "prod_end":
- 
+
                     if (started == true) {
- 
+
                         try (Writer fileWriter = new FileWriter(fname, true)) {
                             fileWriter.append(prod_id + "," + title + "," + price + "," + rating);
                             fileWriter.append(System.getProperty("line.separator"));
@@ -174,26 +155,25 @@ public class tasktwo {
                     started = false;
                     break;
                 default:
-                System.out.println("end of items list");
-                receiveing=false;
+                    System.out.println("end of items list");
+                    receiveing = false;
                     break;
             }
         }
         System.out.println("Preparing ");
-        //Preparing items list
+
         try (FileReader fr = new FileReader(fname)) {
             BufferedReader br1 = new BufferedReader(fr);
-            // System.err.println(br.readLine());
             Items_receive = br1.lines()
-                    // String -> String[]
+
                     .map(row -> row.trim().split(","))
-                    // String[] -> Book
-                    .map(fields -> new Items(Integer.parseInt(fields[0]), fields[1],Float.parseFloat(fields[2]),Float.parseFloat(fields[3])))
-                    // groupingBy -> returns a value that classifies the input
+
+                    .map(fields -> new Items(Integer.parseInt(fields[0]), fields[1], Float.parseFloat(fields[2]),
+                            Float.parseFloat(fields[3])))
+
                     .collect(Collectors.groupingBy(Category -> Category.getProd_id()));
         }
- 
-        
+
         is.close();
         os.close();
         socket.close();
